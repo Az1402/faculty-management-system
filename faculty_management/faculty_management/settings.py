dd0eeb4555622e16f05ac479c3c9ebf1f2bea08c
SomTakalkar <somtakalkar200211@gmail.com>
2024-08-30 13:51:47 +0530

    Tailwind Css

--- faculty_management/faculty_management/settings.py
+++ faculty_management/faculty_management/settings.py
@@ -130,7 +130,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# Optionally, you can specify additional directories to look for static files:
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # For a global static directory
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
