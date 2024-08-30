7bc7c34ea15c3f5b42b8234a9267da734c4895ce
Az1402 <atharvrzalte@gmail.com>
2024-08-30 13:39:33 +0530

    Password setup

--- faculty_management/main/urls.py
+++ faculty_management/main/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('send_message/', views.send_message, name='send_message'),
     path('submit_complaint/', views.submit_complaint, name='submit_complaint'),
-    path('login/', views.login, name='login'),  # Ensure this line is correct
-    path('signup/', views.signup, name='signup'),
+    path('login/', views.login_view, name='login'),  # Updated to match the view name
+    path('signup/', views.signup_view, name='signup'),
     path('logout/', views.logout_view, name='logout'),
 ]
